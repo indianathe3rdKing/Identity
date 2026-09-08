419f2a8d3377563c116d609873ecb6ad9298e0df
indianathe3rd <indiemenziwa@gmail.com>
2026-09-09 00:59:44 +0200

    Update Bump version step in push workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.indianathe3rd.identity"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
