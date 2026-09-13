fafe6ccc163d665ce5aa980b0d96c20178a688d1
indianathe3rd <indiemenziwa@gmail.com>
2026-09-14 01:00:32 +0200

    Add an AppUsage usacase

--- app/src/main/java/com/indianathe3rd/identity/domain/usecase/usage/GetAppsUsageUsecase.kt
+++ app/src/main/java/com/indianathe3rd/identity/domain/usecase/usage/GetAppsUsageUsecase.kt
@@ -1,4 +1,10 @@
 package com.indianathe3rd.identity.domain.usecase.usage
 
-class GetAppsUsageUsecase {
+import com.indianathe3rd.identity.domain.model.AppUsage
+import com.indianathe3rd.identity.domain.repository.UsageRepository
+
+class GetAppsUsageUsecase(
+    private val repository: UsageRepository
+) {
+    suspend operator fun invoke(beginTime: Long,endTime: Long,intervalType:Int):List<AppUsage> = repository.getAppsUsage(beginTime,endTime,intervalType)
 }
